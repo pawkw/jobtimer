3deccae53f6b992d923b26ebba433000842c89ba
pawkw <pawkw@users.noreply.github.com>
2022-05-06 01:23:24 -0400

    Added billing.

--- api/models.py
+++ api/models.py
@@ -14,10 +14,18 @@ class Company(models.Model):
     defaultRate = models.DecimalField(max_digits=20, decimal_places=2)
     created_on = models.DateTimeField(auto_now_add=True)
 
+class Bill(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    company = models.ForeignKey(Company, on_delete=CASCADE)
+    date = models.DateField()
+    # make embedded serializer with company not billed.
+
 class Timer(models.Model):
     user = models.ForeignKey(User, on_delete=CASCADE)
     start = models.DateTimeField()
     end = models.DateTimeField()
     billed = models.BooleanField(default=False)
+    billedOn = models.ForeignKey(Bill, null=True, default=None)
     note = models.TextField()
     specialRate = models.DecimalField(max_digits=20, decimal_places=2, null=True, default=NULL)
+
